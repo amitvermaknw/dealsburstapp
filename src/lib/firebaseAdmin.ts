092414934642c2d2bd5d8cc5d14b9bc00c2ec61a
Amit Verma <amitfromus@gmail.com>
2025-02-27 16:36:39 -0700

    updated the admin and client firebase keys

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -1,10 +1,16 @@
 import * as admin from 'firebase-admin';
+import getConfig from 'next/config';
 
+const { serverRuntimeConfig } = getConfig();
 
 if (!admin.apps.length) {
     admin.initializeApp({
         credential: admin.credential.cert({
-            projectId: process.env.NODE_ENV
+            projectId: process.env.NODE_ENV === 'development' ? process.env.DEALSBURST_PUBLIC_FIREBASE_PROJECT_ID : serverRuntimeConfig.DEALSBURST_PUBLIC_FIREBASE_PROJECT_ID,
+            clientEmail: process.env.NODE_ENV === 'development' ? process.env.DEALSBURST_PRIVATE_FIREBASE_CLIENT_EMAIL : serverRuntimeConfig.DEALSBURST_PRIVATE_FIREBASE_CLIENT_EMAIL,
+            privateKey: process.env.NODE_ENV === 'development' ? process.env.DEALSBURST_PRIVATE_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n") : serverRuntimeConfig.DEALSBURST_PRIVATE_FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n")
         })
     })
 }
+
+export const adminAuth = admin.auth();
